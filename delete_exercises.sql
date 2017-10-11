6f5289869bfb45ea2dd840e94b60bf87996bf158
Art <art_jr2000@yahoo.com>
2017-10-11 14:50:28 -0500

    changed SELECTS to DELETE - DELETED albums released after 1991 - DELETED albums with the genre DISCO - DELETED albums by Whitney Houston

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -5,12 +5,21 @@ SELECT name
 FROM albums
 WHERE release_date > 1991;
 
+DELETE FROM albums
+WHERE release_date > 1991;
+
 SELECT 'Albums with the genre Disco' AS '';
 SELECT name
 FROM albums
 WHERE genre = 'Disco';
 
+DELETE FROM albums
+WHERE genre = 'Disco';
+
 SELECT 'Albums by Whitney Houston' AS '';
 SELECT name
 FROM albums
+WHERE artist = 'Whitney Houston';
+
+DELETE FROM albums
 WHERE artist = 'Whitney Houston';
