4b1238b3a6486b6674eabb3f784a459e2f17b80f
Art <art_jr2000@yahoo.com>
2017-10-12 14:48:01 -0500

    created limit_exercises.sql -completed exercises -edited order_by

---
+++ limit_exercises.sql
@@ -0,0 +1,14 @@
+USE employees;
+
+SELECT DISTINCT last_name
+FROM employees
+GROUP BY last_name DESC
+LIMIT 10 ;
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC ,hire_date DESC
+LIMIT 5
+OFFSET 45;
