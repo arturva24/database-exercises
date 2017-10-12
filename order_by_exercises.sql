dc6e0eeac57702d00de53705faafc023b2ce7c34
Art <art_jr2000@yahoo.com>
2017-10-12 14:00:05 -0500

    CREATED order_by_exercises -copied where_exercises into file and made changes

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -6,24 +6,28 @@ FROM employees
 WHERE (first_name = 'Irena'
 OR first_name = 'Vidya'
 OR first_name = 'Maya')
-AND gender = 'M';
+AND gender = 'M'
+ORDER BY last_name ASC,first_name ASC ;
 
 SELECT 'Employees hired in the 90s AND born on Christmas';
 SELECT *
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31'
-AND birth_date LIKE '%-12-25';
+AND birth_date LIKE '%-12-25'
+ORDER BY hire_date DESC ;
 
 SELECT 'Employees whose last name starts with ''E''';
 SELECT *
 FROM employees
 WHERE last_name LIKE 'e%'
-AND last_name LIKE '%e';
+AND last_name LIKE '%e'
+ORDER BY emp_no ASC;
 
 SELECT *
 FROM employees
 WHERE last_name LIKE 'e%'
-AND last_name like '%e';
+AND last_name like '%e'
+ORDER BY emp_no DESC;
 
 SELECT 'Employees born on Christmas';
 SELECT *
